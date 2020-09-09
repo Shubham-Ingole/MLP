5f965c56bd43cc59994de45283f49614cac6dfff
Shubham-Ingole <71011763+Shubham-Ingole@users.noreply.github.com>
2020-09-09 20:24:34 +0530

    Update model4.py

--- model4.py
+++ model4.py
@@ -77,10 +77,4 @@ api.add_resource(Predict,'/predict')
 
 
 if __name__ == '__main__':
-    import os
-    HOST = os.environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+    app.run(PORT,debug=True)
